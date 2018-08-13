97ed1dacf37484e4b5fdf268e3dc49ae19c44446
ekta.yadav <ekta.yadav@VS2013SW101>
2018-08-13 12:05:53 +0530

    Add project files.

---
+++ AzureDemoApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureDemoApplication.Startup))]
+namespace AzureDemoApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
